afb3e805ba09725536cd4647a1383a8428c2f016
Mangayarkarasi Jeyachandran <mangayarkarasi.j@GAVSTECH.COM>
2020-07-30 00:32:26 +0530

    removed openweatherapi key

--- Week 2 XMLHttp/Open weather API/main1.js
+++ Week 2 XMLHttp/Open weather API/main1.js
@@ -1,5 +1,5 @@
 var httprequest = new XMLHttpRequest();
-httprequest.open('GET', 'https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London,IN-TN&appid=23b478f6cc75a0158cc8e0ceb009b7e9', true);
+
 httprequest.send();
 
 httprequest.onreadystatechange = function()
